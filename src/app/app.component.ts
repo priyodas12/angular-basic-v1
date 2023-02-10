acfc957d9917d6d85012f80707837af7c2baeec8
Priyobrato.Das <priyodas12@gmail.com>
2023-02-10 21:06:37 +0530

    angular app 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-basic-v1';
+  date: string = new Date().toUTCString();
 }

720795eff7ee927deefe443aee8ffe8e91cbee0a
Priyobrato.Das <priyodas12@gmail.com>
2023-02-11 21:09:02 +0530

    headers components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeadersComponent } from './headers/headers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContainerComponent
+    ContainerComponent,
+    NavigationComponent,
+    HeadersComponent
   ],
   imports: [
     BrowserModule,

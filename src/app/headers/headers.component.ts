5901056544045c95a4d272be641d5e2bb964c86d
Priyobrato.Das <priyodas12@gmail.com>
2023-02-23 23:18:52 +0530

    property binding

--- src/app/headers/headers.component.ts
+++ src/app/headers/headers.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-headers',
   templateUrl: './headers.component.html',
-  styleUrls: ['./headers.component.css']
+  styleUrls: ['./headers.component.css'],
 })
-export class HeadersComponent {
+export class HeadersComponent implements OnInit {
+  slogan: string = 'Shpping Window for a common citizen';
+  products: string[] = ['sofa', 'TV', 'Bed', 'Wrodrobe'];
+  product: string = '';
+  productPrice: number = 0;
+  randomPrice: any;
+  imgSource: string = '/assets/shopping.png';
 
+  ngOnInit() {
+    // this.getPrice();
+    // this.randomPrice = setInterval(() => {
+    //   this.getPrice();
+    // }, 1000);
+  }
+
+  getPrice() {
+    this.product =
+      this.products[Math.floor(Math.random() * this.products.length)];
+    this.productPrice = Math.floor(Math.random() * 100000);
+    return this.product + '  : ' + this.productPrice;
+  }
 }
